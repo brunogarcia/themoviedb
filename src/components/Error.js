c6c4a5429d1f501dc3ce5498757018096a3d090a
Bruno Garcia <bruno@garciaechegaray.com>
2018-05-15 20:02:47 +0200

    Add icon to Error component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Error.css';
+import Icon from './Icon';
 
 const Error = () => {
   const message = 'Looks like there was a problem.';
 
   return (
     <div className="Error-main">
+      <p className="Error-icon">
+        <Icon name="alert" />
+      </p>
       <p>{message}</p>
     </div>
   );
