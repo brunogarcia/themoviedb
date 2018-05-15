87383707d3abae46ad6d0209b76eb01ee5687624
Bruno Garcia <bruno@garciaechegaray.com>
2018-05-15 19:41:49 +0200

    Fixed linter errors in constants file

--- src/constants.js
+++ src/constants.js
@@ -1,31 +1,30 @@
 export default {
-    APP: {
-      PATH: {
-        HOME: '/',
-        RESULTS: '/results/:query',
-        DETAILS: '/details/:id'
-      }
+  APP: {
+    PATH: {
+      HOME: '/',
+      RESULTS: '/results/:query',
+      DETAILS: '/details/:id',
     },
-    API: {
-      HOST: 'https://api.themoviedb.org/3',
-      // TODO: move this key to .env file
-      KEY: '2fc1c5cdbc3e418d12cc5110714a82eb',
-      PATH: {
-        POPULAR: 'movie/popular',
-        SEARCH: 'search/movie',
-        MOVIE: 'movie',
-      },
+  },
+  API: {
+    HOST: 'https://api.themoviedb.org/3',
+    // TODO: move this key to .env file
+    KEY: '2fc1c5cdbc3e418d12cc5110714a82eb',
+    PATH: {
+      POPULAR: 'movie/popular',
+      SEARCH: 'search/movie',
+      MOVIE: 'movie',
     },
-    IMAGE: {
-      HOST: 'http://image.tmdb.org/t/p/',
-      DEFAULT: {
-        SMALL: 'https://dummyimage.com/185x278/eeeeee/999999&text=No+poster',
-        LARGE: 'https://dummyimage.com/300x450/eeeeee/999999&text=No+poster',
-      },
-      SIZE: {
-        SMALL: 'w185',
-        LARGE: 'w300',
-      }
-  }
-}
-  
+  },
+  IMAGE: {
+    HOST: 'http://image.tmdb.org/t/p/',
+    DEFAULT: {
+      SMALL: 'https://dummyimage.com/185x278/eeeeee/999999&text=No+poster',
+      LARGE: 'https://dummyimage.com/300x450/eeeeee/999999&text=No+poster',
+    },
+    SIZE: {
+      SMALL: 'w185',
+      LARGE: 'w300',
+    },
+  },
+};
